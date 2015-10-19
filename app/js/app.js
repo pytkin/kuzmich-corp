34d3f755fbe9b011999ac93721ce1ee829a309b5
Dmitry Pytkin <tutbaker@gmail.com>
2015-10-20 02:07:20 +0600

    Add collapsible text to main page; Add error state for .form-control

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
 $(function () {
-
+  //
+  $('.js-text-toggle-link').on('click', function (event) {
+    event.preventDefault();
+    if ($(this).hasClass('active')) {
+      $(this).closest('.block-text-wrapper').css('max-height', '115px');
+      $(this).removeClass('active').text('Подробнее >>');
+    } else {
+      $(this).addClass('active').text('Свернуть >>');
+      $(this).closest('.block-text-wrapper').css('max-height', '500px');
+    }
+  });
 });
 
